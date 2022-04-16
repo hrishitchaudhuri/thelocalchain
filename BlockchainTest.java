eee1922df0dec033cbb787246b8cda871031ab88
ishita1864 <62196026+ishita1864@users.noreply.github.com>
2022-04-16 17:08:03 +0530

    Add files via upload

--- BlockchainTest.java
+++ BlockchainTest.java
@@ -6,31 +6,33 @@ public class BlockchainTest {
         ArrayList<User> usersList = new ArrayList<>();
         int flag=1;
         MemPool mp = new MemPool(1);
-        
+        int existing_users=0;
+        int total_users=0;
+
         while (flag!=0)
         {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of users:");
-        int no_of_users = sc.nextInt();
+        int new_users = sc.nextInt();
         sc.nextLine();
         System.out.println("Enter the users:");
-        for (int i = 0; i < no_of_users; i++) {
+        for ( int i = 0; i < new_users; i++) {
             String user_name = sc.nextLine();
             usersList.add(new User(user_name, mp));
           }
+        total_users=existing_users+new_users;
         
         // System.out.println("Enter the no of transactions:");
         // int no_of_trans = sc.nextInt();
         // sc.nextLine(); 
         System.out.println("Enter the corresponding transaction name, and transaction details in the nextline:");
-        for (int i = 0; i < no_of_users; i++) {
+        for (int i = existing_users; i < total_users; i++) {
             String transacName = sc.nextLine();
             String transac= sc.nextLine();
             usersList.get(i).setTransaction(transac, transacName);
 
-            // System.out.println(transacName);
+            System.out.println("The anee is -" +usersList.get(i).getUserName());
           }
-
 
         Miner man = new Miner();
         
@@ -40,7 +42,7 @@ public class BlockchainTest {
         bc.displayAll();
         System.out.println("Do u wanna exit: 0. Yes 1. No");
         flag=sc.nextInt();
-        
+        existing_users=total_users;
         }
     }
 }
